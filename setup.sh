90d3f4d13d15b8b85ebbb34031dbcb8b3fe3a25b
Steven D. Shelton <Steven-Shelton@UTC.edu>
2020-03-05 16:24:09 -0500

    updated files from old Mac

--- setup.sh
+++ setup.sh
@@ -5,22 +5,34 @@ yum -y update
 echo "Installing wget ..."
 yum -y install wget
 #install MW if not already installed
-if  [ -e /var/www/html/index.php ]
-then
-echo "MediaWiki is already installed."
-else
-echo "Installing MediaWiki ..."
-wget https://releases.wikimedia.org/mediawiki/1.32/mediawiki-1.32.2.tar.gz
-tar xf  mediawiki*.tar.gz
-mv mediawiki-1.32.2/* /var/www/html/
-fi
+# if  [ -e /var/www/html/index.php ]
+# then
+# echo "MediaWiki is already installed."
+# else
+# echo "Installing MediaWiki ..."
+# wget https://releases.wikimedia.org/mediawiki/1.32/mediawiki-1.32.2.tar.gz
+# tar xvzf  mediawiki*.tar.gz
+# mv mediawiki-1.32.2/* /var/www/html/
+# fi
 echo "Installing editors ..."
 yum -y install nano vim
-echo "Installing PHP 7 ..."
+echo "Installing PHP ..."
 yum -y install epel-release yum-utils
 yum -y install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
 yum-config-manager --enable remi-php73
-yum -y install php php-mcrypt php-cli php-gd php-curl php-mysql php-ldap php-zip php-fileinfo php-xml php-mbstring
+yum -y install php php-mcrypt php-cli php-gd php-curl php-mysql php-ldap php-zip php-fileinfo php-xml php-mbstring uzip
+# echo "installing Composer and bootstrap for mediawiki" # see https://www.vultr.com/docs/install-composer-on-centos-7
+# curl -sS https://getcomposer.org/installer | php
+# mv composer.phar /usr/local/bin/composer
+# composer update --no-dev "mediawiki/bootstrap"
+# install MediaWikiBootstrap
+# echo "mediawiki/bootstrap: ~4.0" > /var/www/html/composer.local.json
+# cd /var/www/html
+# composer update --no-dev "mediawiki/bootstrap"
+# echo
+yum -y install gcc php-devel php-pear
+yum -y install ImageMagick ImageMagick-devel ImageMagick-perl
+
 echo "Installing Apache ..."
 yum -y install httpd
 echo "Modify php.ini to show php errors"
@@ -31,12 +43,14 @@ echo "Installing MariaDB ..."
 yum -y install mariadb-server
 systemctl start mariadb
 systemctl status mariadb
-if ! mysql -u root -e 'USE media_wiki'; then
-mysql -u root -e "CREATE database media_wiki";
+if ! mysql -u root -e 'USE wiki_lib'; then
+mysql -u root -e "CREATE database wiki_lib";
 #mysql -u root LuptonDB < /vagrant/mw.sql;
-mysql -u root -e "CREATE USER 'media_wiki'@'localhost' identified by 'mw123'";
-mysql -u root -e "GRANT ALL PRIVILEGES ON media_wiki.* TO 'media_wiki'@'localhost'";
+mysql -u root -e "CREATE USER 'wikiadmin'@'localhost' identified by 'Fr3sh1nstall'";
+mysql -u root -e "GRANT ALL PRIVILEGES ON wiki_lib.* TO 'wikiadmin'@'localhost' WITH GRANT OPTION";
 fi
+mysql -u root wiki_lib < /vagrant/wiki_lib.sql;
+php /var/www/html/maintenance/update.php
 echo "Installing git ..."
 yum -y install git
 #echo "Getting our git files"
@@ -49,5 +63,5 @@ yum -y install git
 #fi
 
 setenforce 0
-echo "use 192.168.33.15 OR localhost:8080"
-echo "media_wiki mw123"
+echo "use 192.168.33.21 OR localhost:8080"
+echo "wikiadmin Fr3sh1nstall"
